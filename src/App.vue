c2201fc9bcc1c50ef04713748295808be25c6eb9
cokoyoh <charlesokoyoh@gmail.com>
2017-11-18 16:02:18 +0300

    custom directives completed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
     <div id = "app" class = "app">
         <div class="large-8 medium-8 large-offset-2 medium-offset-2 columns">
-            <h2>All Blogs Here</h2>
-            <div class="blogs" v-for="blog in blogs">
-                <div class="panel">
-                    <h4 v-rainbow>{{blog.title}}</h4>
-                    <p>{{blog.body}}</p>
+            <div class="all-blogs" v-theme:column="'wide'">
+                <h2>All Blogs Here</h2>
+                <div class="blogs" v-for="blog in blogs">
+                    <div class="panel">
+                        <h4 v-rainbow>{{blog.title}}</h4>
+                        <p>{{blog.body}}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//Custom directives defined here
-Vue.directive('rainbow',{
-    bind(el, binding, vnode){
+//Custom directives defined here for h4 heading
+Vue.directive('rainbow', {
+    bind(el, binding, vnode) {
         //pick a string of 6 random numbers and attache it to the styling
-        el.style.color = '#' + Math.random().toString().slice(2,8);
+        el.style.color = '#' + Math.random().toString().slice(2, 8);
+    }
+});
+
+Vue.directive('theme', {
+    bind(el, binding, vnode) {
+       if(binding.value == 'wide'){
+            el.style.maxWidth = "1200px";
+       } else if(binding.value == 'narrow'){
+           el.style.maxWidth = "560px";
+       }
+       if(binding.arg == 'column'){
+           // el.style.background = "gray";
+           el.style.opacity = "0.9";
+           el.style.padding = "20px";
+       }
     }
 });
 
